299951691ae8ba722e431b839253e96fc9ee87cd
Lukáš Kotržena <lkotrzena@gmail.com>
2023-05-01 14:08:43 +0200

    Generate no scripts version with textures in subdir

--- install.ps1
+++ install.ps1
@@ -31,6 +31,7 @@ Get-ChildItem -Path ".\$mod_name\*" -File -Recurse | ForEach-Object {
 
 # No scripts version
 $game_dir = "D:\Hry\steamapps\common\SpaceEngineers";
+$textures_sub_dir = "Colorful Icons";
 $mod_dir_no_scripts = "$mods_dir\$($mod_name)NoScripts";
 if(Test-Path -Path "$mod_dir_no_scripts"){
 	Remove-Item -Recurse "$mod_dir_no_scripts";
@@ -39,7 +40,7 @@ New-Item -ItemType "directory" "$mod_dir_no_scripts" | Out-Null;
 Copy-Item "$mod_dir\thumb.png" "$mod_dir_no_scripts\";
 Copy-Item "$mod_dir\metadata.mod" "$mod_dir_no_scripts\";
 Copy-Item "$mod_dir\modinfo.sbmi" "$mod_dir_no_scripts\";
-Copy-Item -Recurse "$mod_dir\Textures" "$mod_dir_no_scripts\Textures";
+Copy-Item -Recurse "$mod_dir\Textures" "$mod_dir_no_scripts\Textures\$textures_sub_dir";
 
 New-Item -ItemType "directory" "$mod_dir_no_scripts\Data";
 
@@ -50,3 +51,76 @@ Copy-Item "$game_dir\Content\Data\PhysicalItems.sbc" "$mod_dir_no_scripts\Data\"
 Copy-Item "$game_dir\Content\Data\PhysicalItems_Economy.sbc" "$mod_dir_no_scripts\Data\";
 Copy-Item "$game_dir\Content\Data\Weapons.sbc" "$mod_dir_no_scripts\Data\";
 Copy-Item -Recurse "$game_dir\Content\Data\CubeBlocks" "$mod_dir_no_scripts\Data\";
+
+$config = Get-Content -Path .\ColorfulItems\Data\Scripts\Sisk\Config.cs -Raw
+
+Add-Type -TypeDefinition $config -Language CSharp
+
+$sbcFiles = @(
+	"AmmoMagazines.sbc",
+	"Ammos.sbc",
+	"BlockVariantGroups.sbc",
+	"Components.sbc",
+	"PhysicalItems.sbc",
+	"PhysicalItems_Economy.sbc",
+	"Weapons.sbc",
+	"CubeBlocks/CubeBlocks.sbc",
+	"CubeBlocks/CubeBlocks_Armor.sbc",
+	"CubeBlocks/CubeBlocks_ArmorPanels.sbc",
+	"CubeBlocks/CubeBlocks_Armor_2.sbc",
+	"CubeBlocks/CubeBlocks_Armor_3.sbc",
+	"CubeBlocks/CubeBlocks_Automation.sbc",
+	"CubeBlocks/CubeBlocks_Communications.sbc",
+	"CubeBlocks/CubeBlocks_Control.sbc",
+	"CubeBlocks/CubeBlocks_DecorativePack.sbc",
+	"CubeBlocks/CubeBlocks_DecorativePack2.sbc",
+	"CubeBlocks/CubeBlocks_Doors.sbc",
+	"CubeBlocks/CubeBlocks_Economy.sbc",
+	"CubeBlocks/CubeBlocks_Energy.sbc",
+	"CubeBlocks/CubeBlocks_Extras.sbc",
+	"CubeBlocks/CubeBlocks_Frostbite.sbc",
+	"CubeBlocks/CubeBlocks_Gravity.sbc",
+	"CubeBlocks/CubeBlocks_GridAIPack.sbc",
+	"CubeBlocks/CubeBlocks_IndustrialPack.sbc",
+	"CubeBlocks/CubeBlocks_Interiors.sbc",
+	"CubeBlocks/CubeBlocks_LCDPanels.sbc",
+	"CubeBlocks/CubeBlocks_Lights.sbc",
+	"CubeBlocks/CubeBlocks_Logistics.sbc",
+	"CubeBlocks/CubeBlocks_Mechanical.sbc",
+	"CubeBlocks/CubeBlocks_Medical.sbc",
+	"CubeBlocks/CubeBlocks_Production.sbc",
+	"CubeBlocks/CubeBlocks_ScrapRacePack.sbc",
+	"CubeBlocks/CubeBlocks_SparksOfTheFuturePack.sbc",
+	"CubeBlocks/CubeBlocks_Symbols.sbc",
+	"CubeBlocks/CubeBlocks_Thrusters.sbc",
+	"CubeBlocks/CubeBlocks_Tools.sbc",
+	"CubeBlocks/CubeBlocks_Utility.sbc",
+	"CubeBlocks/CubeBlocks_Warfare1.sbc",
+	"CubeBlocks/CubeBlocks_Warfare2.sbc",
+	"CubeBlocks/CubeBlocks_Weapons.sbc",
+	"CubeBlocks/CubeBlocks_Wheels.sbc",
+	"CubeBlocks/CubeBlocks_Windows.sbc"
+);
+
+function replace_icons_paths {
+	param (
+		$content,
+		$paths
+	)
+	foreach ($value in $paths){
+		$icon_path = $value -replace "/", "\";
+		$icon_path_mod = $icon_path -replace "Textures\\", "Textures\$textures_sub_dir\";
+		$content = ($content -replace [Regex]::Escape($icon_path), $icon_path_mod);
+	}
+	return $content;
+}
+
+foreach ($sbc in $sbcFiles){
+	$sbcContent = Get-Content -Path "$game_dir\Content\Data\$sbc";
+	$sbcContent = replace_icons_paths $sbcContent $([Sisk.ColorfulIcons.Config]::Components.Values);
+	$sbcContent = replace_icons_paths $sbcContent $([Sisk.ColorfulIcons.Config]::Blocks.Values);
+	$sbcContent = replace_icons_paths $sbcContent $([Sisk.ColorfulIcons.Config]::Ingots.Values);
+	$sbcContent = replace_icons_paths $sbcContent $([Sisk.ColorfulIcons.Config]::Ores.Values);
+	$sbcContent = replace_icons_paths $sbcContent $([Sisk.ColorfulIcons.Config]::Tools.Values);
+	Set-Content -Path "$mod_dir_no_scripts\Data\$sbc" -Value $sbcContent -Encoding UTF8
+}
